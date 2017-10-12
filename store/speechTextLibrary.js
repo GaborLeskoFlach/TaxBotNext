9c99b1c0dbf685141383588eb5e97e0b3698dbe3
Gabor Lesko-Flach <gabor.lesko-flach@xero.com>
2017-10-12 14:30:27 +1100

    initial commit

---
+++ store/speechTextLibrary.js
@@ -0,0 +1,11 @@
+const welcome_what_is_your_name = 'Hello my name is Cortana...what is your name?'
+const welcome_still_waiting_for_input = 'I am still here and waiting for your input'
+
+const welcome_what_can_i_do = 'So this is what I can help you with @name, please select/say what you would like me to do next'
+
+
+module.exports = {
+    welcome_what_is_your_name,
+    welcome_still_waiting_for_input,
+    welcome_what_can_i_do
+}

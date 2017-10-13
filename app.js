77ae5ad9f96190665780a4309f802d7e47ec52ea
Gabor Lesko-Flach <gabor.lesko-flach@xero.com>
2017-10-13 12:49:22 +1100

    api added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var speechTextLib = require('./store/speechTextLibrary')
 var ssml = require('./ssml');
+var fetch = require('node-fetch')
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -133,7 +134,7 @@ bot.dialog('nominatedFor_selector', [
     (session, results) => {
         session.say('Please bear with me till I gather some data','Please bear with me till I gather some data')
         
-        getNominationCategories().then((response) => {
+        getAwards().then((response) => {
             session.userData.nominationChoices = response
             if(session.userData.nominationChoices){
                 builder.Prompts.choice(session, `All done, now please select or say which option you would like me to proceed with?`, session.userData.nominationChoices, {
@@ -245,6 +246,9 @@ bot.dialog('nominate', [
                     retrySpeak: "Still here waiting for your input",
                     inputHint: builder.InputHint.expectingInp                    
                 });
+            }).catch((err) => {
+                session.say('Oops, something went wrong', 'Oops, something went wrong')
+                session.replaceDialog('menu')
             })
 
         }else{
@@ -277,6 +281,9 @@ bot.dialog('menu', [
     (session, results) => {
         if(results.response.entity){
             switch(results.response.entity){
+                case "0":
+                    session.replaceDialog('quick_nomination')
+                    break                
                 case "1":
                     session.replaceDialog('greetings')
                     break
@@ -298,6 +305,74 @@ bot.dialog('menu', [
         }
     }
 ]).triggerAction({ matches: /menu/i });
+
+bot.dialog('quick_nomination', [
+    (session) => {
+
+        builder.Prompts.text(session, "Please say in one sentence who you would like to nominate for what award", {
+            speak: "Please say in one sentence who you would like to nominate for what award",
+            retrySpeak: speechTextLib.welcome_still_waiting_for_input,
+            inputHint: builder.InputHint.acceptingInput
+        })
+    },
+    (session,results) => {
+        if(results.response){
+            
+            const text = results.response
+            const temp = text.toLowerCase().replace('i would like to nominate','').replace("i'd like to nominate")
+            const username = temp.substring(0, temp.indexOf('for')).trim()
+            const award = temp.toLowerCase().replace(username,'').replace('for','').trim()
+
+            if(username && award){
+                builder.Prompts.confirm(session, "You can also post a message along with your nomination. It is optional. Would you like to do that?",{
+                    speak: "You can also post a message along with your nomination. It is optional. Would you like to do that?",
+                    retrySpeak: "Still here waiting for your input",
+                    inputHint: builder.InputHint.expectingInput
+                });    
+            }else{
+
+            }
+
+            //session.say(`User to nominate for ${award} is ${username}`)
+        }
+    },
+    (session, results) => {
+        if(results.response === true){
+            builder.Prompts.text(session, "Please write your message", {
+                speak: "Please say your message",
+                retrySpeak: speechTextLib.welcome_still_waiting_for_input,
+                inputHint: builder.InputHint.acceptingInput
+            })         
+        }else{
+            session.say('Nomination is done')
+        }
+    },
+    (session, results) => {
+        if(results.response){
+            session.say('Thanks. Posting Nomination now...','Thanks. Posting Nomination now...')
+
+            nominate(session).then((response) => {
+                builder.Prompts.confirm(session, "All done, nomination has been posted. Is there anything else I can help you with?", {
+                    speak: "All done, nomination has been posted. Is there anything else I can help you with?",
+                    retrySpeak: "Still here waiting for your input",
+                    inputHint: builder.InputHint.expectingInp                    
+                });                
+            }).catch((err) => {
+                session.say('Oops, something went wrong', 'Oops, something went wrong')
+                session.replaceDialog('menu')
+            })
+        }
+    },
+    (session, results) => {
+        if(results.response === true){
+            session.replaceDialog('menu')
+        }else{
+            session.endConversation(`See you another day ${session.userData.username}`, {
+                speak : 'Bye bye'
+            })
+        }
+    }     
+])
 
 bot.dialog('get_my_nominations', [
     (session, args) => {
@@ -353,6 +428,7 @@ bot.dialog('help', function (session) {
 /*********************************** HELPERS ************************************/
 
 var supportedFunctions = [
+    { value: '0', action: { title: 'Quick Nomination' }, synonyms: 'zero|null|quick nomination|nomination|quicky|quick one' },
     { value: '1', action: { title: 'Nominate a Team or Individual for an award' }, synonyms: 'one|nominate|nominate a team|nominate an individual|nominate for award' },
     { value: '2', action: { title: 'Get my nominations' }, synonyms: 'two|too|get nominations|get my nominations|my nominations' },
     { value: '3', action: { title: 'Get all users to nominate' }, synonyms: 'three|tree|get users|get all users|get users to nominate|get all users to nominate' },
@@ -388,8 +464,10 @@ function getNomineeType(){
        
 }
 
-function getNominationCategories(){
+function getAwards(){    
     return new Promise((resolve,reject) => {
+
+        /*
         var choices = [
             { value: '1', action: { title: 'Nominate for Cleanest Desk award' }, synonyms: 'one|cleanest desk award' },
             { value: '2', action: { title: 'Nominate for Cleanest Mug award' }, synonyms: 'two|too|cleanest mug award' },
@@ -397,13 +475,53 @@ function getNominationCategories(){
             { value: '4', action: { title: 'Nominate for Cleanest Screen award' }, synonyms: 'four|for|cleanest screen award' },
         ]
     
-        setTimeout(() => resolve(choices), 3000);        
+        setTimeout(() => resolve(choices), 3000);
+        */
+
+        fetch('http://xlw-cnd70778yz.inside.xero.com/api/awards', 
+        { 
+            method: 'GET', 
+            headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+        })
+        .then((res) => {
+            return res.json();
+        }).then((json) => {
+            resolve(json)
+        }).catch((err) => {            
+            reject(err)
+        })
+
+
     })
 }
 
-function nominate(){
+function nominate(entity){
     return new Promise((resolve, reject) => {
-        setTimeout(() => { resolve() }, 5000);
+
+        var body = {
+            awardId : entity.awardId,
+            awardNomineeId : entity.awardNomineeId,
+            message : entity.message,
+            labels : entity.label
+        }
+
+        var z = JSON.stringify(body)
+        console.log(JSON.stringify(body))
+
+        fetch('http://xlw-cnd70778yz.inside.xero.com/api/votes', 
+        { 
+            method: 'POST', 
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+        })
+        .then((res) => {
+            return res.json();
+        }).then((json) => {
+            resolve(true)
+        }).catch((err) => {            
+            reject(err)
+        })
+
     })
 }
 
@@ -417,3 +535,5 @@ function speak(session, prompt) {
     var localized = session.gettext(prompt);
     return ssml.speak(localized);
 }
+
+
